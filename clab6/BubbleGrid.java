dcdae5d4ac589717d42b5d99ab4eb265c852c924
msnliu <zongjunliu@hsph.harvard.edu>
2021-12-30 22:56:07 -0500

    clab6 finished

---
+++ clab6/BubbleGrid.java
@@ -0,0 +1,84 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class BubbleGrid {
+    private int[][] grid;
+    private int rowNum;
+    private int colNum;
+    private int topmost = 0;
+    private int[][] surroundings = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private int[] result;
+
+    /* Create new BubbleGrid with bubble/space locations specified by grid.
+     * Grid is composed of only 1's and 0's, where 1's denote a bubble, and
+     * 0's denote a space. */
+    public BubbleGrid(int[][] grid) {
+        this.grid = grid;
+        rowNum = grid.length;
+        colNum = grid[0].length;
+    }
+
+    /* Returns an array whose i-th element is the number of bubbles that
+     * fall after the i-th dart is thrown. Assume all elements of darts
+     * are unique, valid locations in the grid. Must be non-destructive
+     * and have no side-effects to grid. */
+    public int[] popBubbles(int[][] darts) {
+        // get to the final state where all darts have been shot
+        for (int[] dart : darts) {
+            if (grid[dart[0]][dart[1]] == 1) {
+                grid[dart[0]][dart[1]] = 2;
+            }
+        }
+
+        // connect the final remaining grids after all darts have been shot
+        UnionFind uf = new UnionFind(rowNum * colNum + 1);
+        for (int i = 0; i < rowNum; i++) {
+            for (int j = 0; j < colNum; j++) {
+                if (grid[i][j] == 1) {
+                    unionSurrounding(i, j, uf);
+                }
+            }
+        }
+
+        int numOfStuck = uf.sizeOf(topmost);
+
+        int i = 0;
+        result = new int[darts.length];
+        for (int j = darts.length - 1; j >= 0; j--) {
+            int[] t = darts[j];
+            // reversely add dart
+            if (grid[t[0]][t[1]] == 2) {
+                grid[t[0]][t[1]] = 1;
+            }
+            unionSurrounding(t[0], t[1], uf);
+            int temp = uf.sizeOf(topmost);
+            // minus pop itself!
+            result[i] = temp - numOfStuck - 1;
+            numOfStuck = temp;
+            i += 1;
+        }
+        return result;
+    }
+
+    public void unionSurrounding(int row, int col, UnionFind uf) {
+        if (row == 0) {
+            uf.connect(topmost, xyto1D(row, col));
+        }
+        for (int[] surrounding : surroundings) {
+            int newrow = row + surrounding[0];
+            int newcol = col + surrounding[1];
+            if (validate(newrow, rowNum) && validate(newcol, colNum)) {
+                if (grid[newrow][newcol] == 1) {
+                    uf.connect(xyto1D(row, col), xyto1D(newrow, newcol));
+                }
+            }
+        }
+    }
+
+    public boolean validate(int dim, int ref) {
+        return dim >= 0 && dim < ref;
+    }
+
+    public int xyto1D(int row, int col) {
+        return row * colNum + col + 1;
+    }
+}
---
+++ clab6/UnionFind.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+
+public class UnionFind {
+    int[] parent;
+
+    /* Creates a UnionFind data structure holding n vertices. Initially, all
+       vertices are in disjoint sets. */
+    public UnionFind(int n) {
+        parent = new int[n];
+        // set all the parents to be -1 to symbolize that they are disjoint
+        for (int i = 0; i < n; i++) {
+            parent[i] = -1;
+        }
+    }
+
+    /* Throws an exception if v1 is not a valid vertex. */
+    private void validate(int v1) {
+        if (v1 < 0 || v1 >= parent.length) {
+            throw new IllegalArgumentException("Not a valid vertex!");
+        }
+    }
+
+    /* Returns the size of the set v1 belongs to. */
+    public int sizeOf(int v1) {
+        validate(v1);
+        int root = find(v1);
+        return -1 * parent[root];
+    }
+
+    /* Returns the parent of v1. If v1 is the root of a tree, returns the
+       negative size of the tree for which v1 is the root. */
+    public int parent(int v1) {
+        validate(v1);
+        return parent[v1];
+    }
+
+    /* Returns true if nodes v1 and v2 are connected. */
+    public boolean isConnected(int v1, int v2) {
+        validate(v1);
+        validate(v2);
+        return find(v1) == find(v2);
+    }
+
+    /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
+       elements, and a union-by-size heuristic is used. If the sizes of the sets
+       are equal, tie break by connecting v1's root to v2's root. Connecting a
+       vertex with itself or vertices that are already connected should not 
+       change the sets but may alter the internal structure of the data. */
+    public void connect(int v1, int v2) {
+        validate(v1);
+        validate(v2);
+        if (!isConnected(v1, v2)) {
+            if (sizeOf(v1) > sizeOf(v2)) {
+                /** update size first, then connect */
+                parent[find(v1)] -= sizeOf(v2);
+                parent[find(v2)] = find(v1);
+            } else {
+                parent[find(v2)] -= sizeOf(v1);
+                parent[find(v1)] = find(v2);
+            }
+        }
+    }
+
+    /* Returns the root of the set v1 belongs to. Path-compression is employed
+       allowing for fast search-time. */
+
+    /** recursive
+    public int find(int v1) {
+        validate(v1);
+        if (parent[v1] >= 0) {
+            return find(parent[v1]);
+        }
+        return v1;
+    }
+    */
+
+    /** iterative */
+    public int find(int v1) {
+        validate(v1);
+        int root = v1;
+        while (parent(root) >= 0) {
+            root = parent(root);
+        }
+        /** Path-compression */
+        int path = v1;
+        while (parent(path) >= 0) {
+            int temp = parent[path];
+            parent[path] = root;
+            path = temp;
+        }
+        return root;
+    }
+}
