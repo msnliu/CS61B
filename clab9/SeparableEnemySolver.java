bfb85225ec9f80a84a85e009c6fa601745ca0382
msnliu <zongjunliu@hsph.harvard.edu>
2022-01-05 01:26:06 -0500

    clab9 finished

---
+++ clab9/SeparableEnemySolver.java
@@ -0,0 +1,129 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class SeparableEnemySolver {
+
+    Graph g;
+
+    /**
+     * Creates a SeparableEnemySolver for a file with name filename. Enemy
+     * relationships are biderectional (if A is an enemy of B, B is an enemy of A).
+     */
+    SeparableEnemySolver(String filename) throws java.io.FileNotFoundException {
+        this.g = graphFromFile(filename);
+    }
+
+    /** Alterntive constructor that requires a Graph object. */
+    SeparableEnemySolver(Graph g) {
+        this.g = g;
+    }
+
+    /**
+     * Returns true if input is separable, false otherwise.
+     */
+    public boolean isSeparable() {
+        // use BFS traversal
+        // create mark (mark if visited)
+        Map<String, Integer> mark = new HashMap<>();
+        // initialize mark
+        for (String i : g.labels()) {
+            mark.put(i, 0);
+        }
+        // initialize empty queue
+        Queue<String> bfs = new LinkedList<>();
+        // do not set up starting key because here not all are possibly connected
+        // want to find the discrepancy in between two bfs
+        // true scenario: two graphs traversals, no interaction
+        // false scenario: interaction
+        for (String i : g.labels()) {
+            /** treat each iteration as a bfs */
+            if (mark.get(i) == 0) {
+                bfs.add(i);
+                mark.put(i, 1);
+            }
+            while (!bfs.isEmpty()) {
+                String temp = bfs.poll();
+                for (String j : g.neighbors(temp)) {
+                    // if !mark
+                    if (mark.get(j) == 0) {
+                        bfs.add(j);
+                        mark.put(j, markopposite(mark.get(temp)));
+                    }
+                    // temp is from bfs, so it has mark
+                    if (mark.get(j) == mark.get(temp)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public int markopposite(int x) {
+        if (x == 2) {
+            return 1;
+        }
+        return 2;
+    }
+
+
+    /* HELPERS FOR READING IN CSV FILES. */
+
+    /**
+     * Creates graph from filename. File should be comma-separated. The first line
+     * contains comma-separated names of all people. Subsequent lines each have two
+     * comma-separated names of enemy pairs.
+     */
+    private Graph graphFromFile(String filename) throws FileNotFoundException {
+        List<List<String>> lines = readCSV(filename);
+        Graph input = new Graph();
+        for (int i = 0; i < lines.size(); i++) {
+            if (i == 0) {
+                for (String name : lines.get(i)) {
+                    input.addNode(name);
+                }
+                continue;
+            }
+            assert(lines.get(i).size() == 2);
+            input.connect(lines.get(i).get(0), lines.get(i).get(1));
+        }
+        return input;
+    }
+
+    /**
+     * Reads an entire CSV and returns a List of Lists. Each inner
+     * List represents a line of the CSV with each comma-seperated
+     * value as an entry. Assumes CSV file does not contain commas
+     * except as separators.
+     * Returns null if invalid filename.
+     *
+     * @source https://www.baeldung.com/java-csv-file-array
+     */
+    private List<List<String>> readCSV(String filename) throws java.io.FileNotFoundException {
+        List<List<String>> records = new ArrayList<>();
+        Scanner scanner = new Scanner(new File(filename));
+        while (scanner.hasNextLine()) {
+            records.add(getRecordFromLine(scanner.nextLine()));
+        }
+        return records;
+    }
+
+    /**
+     * Reads one line of a CSV.
+     *
+     * @source https://www.baeldung.com/java-csv-file-array
+     */
+    private List<String> getRecordFromLine(String line) {
+        List<String> values = new ArrayList<String>();
+        Scanner rowScanner = new Scanner(line);
+        rowScanner.useDelimiter(",");
+        while (rowScanner.hasNext()) {
+            values.add(rowScanner.next().trim());
+        }
+        return values;
+    }
+
+    /* END HELPERS  FOR READING IN CSV FILES. */
+
+}
